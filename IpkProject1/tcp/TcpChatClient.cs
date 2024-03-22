e088eb6be90f3e57411db87dfc39a5a69a530a1d
xshche05 <xshche05@stud.fit.vutbr.cz>
2024-03-22 19:39:27 +0100

    Fixes using block methods, add printer

--- IpkProject1/tcp/TcpChatClient.cs
+++ IpkProject1/tcp/TcpChatClient.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Net.Sockets;
 using System.Text;
 
@@ -5,7 +6,7 @@ namespace IpkProject1.tcp;
 
 public class TcpChatClient
 {
-    public static TcpClient Client { get; } = new TcpClient();
+    private static TcpClient Client { get; } = new TcpClient();
     public static void Connect(string ip, int port)
     {
         Client.Connect(ip, port);
@@ -19,39 +20,62 @@ public class TcpChatClient
     }
     
     // async function send data to server
-    public static async Task SendDataToServer(string msg)
+    public static async Task SendDataToServer(TcpPacket packet)
     {
-        byte[] buffer = Encoding.UTF8.GetBytes(msg);
+        byte[] buffer = packet.ToBytes();
         await Client.GetStream().WriteAsync(buffer, 0, buffer.Length);
     }
     
-    public static async Task Reader(Queue<TcpPacket> gotPackets)
+    public static async Task Reader(BlockingCollection<TcpPacket> gotPackets)
     {
         // read separate messages from server, every message is separated by CRLF
         string message = "";
         byte[] buffer = new byte[1];
         // write to stderr if reader is terminated
         Console.Error.WriteLine("Reader started...");
-        while (Client.Connected)
+        Socket s = Client.Client;
+        while (s.Connected)
         {
-            // if no data available, continue
-            // Client connection check for exclude any exceptions in the future
-            if (Client.Connected && !Client.GetStream().DataAvailable)
+            try
             {
-                continue; // if no data available, continue
-            }
-            while (Client.Connected && (await Client.GetStream().ReadAsync(buffer, 0, buffer.Length)) != 0)
-            {
-                message += Encoding.UTF8.GetString(buffer); // read stream to message
-                if (message.EndsWith("\r\n")) // check if full message is received
+                while (await s.ReceiveAsync(buffer) != 0)
                 {
-                    // Todo parse msg to packet
-                    // add packet to queue
-                    Console.WriteLine(message.Trim());
-                    message = ""; // clear message
+                    message += Encoding.UTF8.GetString(buffer); // read stream to message
+                    if (message.EndsWith("\r\n")) // check if full message is received
+                    {
+                        gotPackets.Add(TcpPacketParser.Parse(message.Trim()));
+                        message = ""; // clear message
+                    }
                 }
             }
+            catch (SocketException e)
+            {
+                Console.Error.WriteLine("Reader got socket closed");
+                break;
+            }
         }
+        gotPackets.CompleteAdding();
         Console.Error.WriteLine("Reader terminated...");
+    }
+    
+    public static async Task Printer(BlockingCollection<TcpPacket> gotPackets)
+    {
+        Console.Error.WriteLine("Printer started...");
+        while (!gotPackets.IsCompleted)
+        {
+            TcpPacket? packet = await Task.Run(() =>
+            {
+                try
+                {
+                    TcpPacket p = gotPackets.Take(); 
+                    return p;
+                } catch (InvalidOperationException)
+                {
+                    return null;
+                }
+            });
+            if (packet != null) packet.Print();
+        }
+        Console.Error.WriteLine("Printer terminated...");
     }
 }
