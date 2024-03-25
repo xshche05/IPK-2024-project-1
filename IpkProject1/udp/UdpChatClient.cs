54d491762ec3120c1d548e9e131d22837502dfd3
xshche05 <xshche05@stud.fit.vutbr.cz>
2024-03-25 20:52:11 +0100

    Udp client

--- IpkProject1/udp/UdpChatClient.cs
+++ IpkProject1/udp/UdpChatClient.cs
@@ -1,6 +1,9 @@
 using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using IpkProject1.enums;
+using IpkProject1.fsm;
 using IpkProject1.interfaces;
 
 namespace IpkProject1.udp;
@@ -8,37 +11,26 @@ namespace IpkProject1.udp;
 public class UdpChatClient : IChatClient
 {
     private readonly UdpClient _client = new ();
-    private readonly BlockingCollection<UdpPacket> _gotPackets = new ();
+    private bool _isConnected = false;
+    private IPEndPoint endPoint;
     private int _idCounter = 0;
-    private List<int> _responseIds = new ();
-    
-    public UdpChatClient()
-    {
-        throw new NotImplementedException();
-    }
+    private BlockingCollection<int> confirmedMessages = new ();
+    private List<int> processedMessages = new ();
     
     public void Connect(string ip, int port)
     {
-        _client.Connect(ip, port);
+        // todo: check if ip is valid or hostname
+        IPAddress ipAddress = Dns.GetHostAddresses(ip)[0];
+        endPoint = new IPEndPoint(ipAddress, port);
+        _client.Client.Bind(new IPEndPoint(IPAddress.Any, 0));
+        _isConnected = true;
+        Console.WriteLine("Connected to server...");
     }
     
-    private void ChangePort(int port)
-    {
-        var endPoint = _client.Client.RemoteEndPoint as IPEndPoint;
-        _client.Close();
-        if (endPoint == null) return;
-        _client.Connect(endPoint.Address.ToString(), port);
-    }
     public void Disconnect()
     {
-        try
-        {
-            _client.Client.Shutdown(SocketShutdown.Both);
-        }
-        finally
-        {
-            _client.Client.Close();
-        }
+        _client.Close();
+        _isConnected = false;
         Console.WriteLine("Disconnected from server...");
     }
 
@@ -47,19 +39,106 @@ public class UdpChatClient : IChatClient
         _idCounter++;
         return _idCounter;
     }
-    
+
     public async Task Reader()
     {
-        throw new NotImplementedException();
+        Console.Error.WriteLine("Reader started...");
+        while (_isConnected)
+        {
+            try
+            {
+                var data = await _client.ReceiveAsync();
+                byte[] bytes = data.Buffer;
+                MessageTypeEnum type = (MessageTypeEnum)bytes[0];
+                UInt16 msg_id = BitConverter.ToUInt16(bytes[1..3]);
+                if (type == MessageTypeEnum.Confirm)
+                {
+                    confirmedMessages.Add(msg_id);
+                }
+                else
+                {
+                    endPoint = data.RemoteEndPoint;
+                    UdpPacket p = UdpPacketBuilder.build_confirm(msg_id);
+                    // randomize confirm message
+                    // if (new Random().Next(0, 20) < 10)
+                    // {
+                    //     await _client.Client.SendToAsync(p.ToBytes(), SocketFlags.None, endPoint);   
+                    // }
+                    await _client.Client.SendToAsync(p.ToBytes(), SocketFlags.None, endPoint);
+                    UdpPacket packet = new UdpPacket(type, bytes);
+                    if (!processedMessages.Contains(msg_id))
+                    {
+                        processedMessages.Add(msg_id);
+                        packet.Print();
+                        switch (ClientFsm.GetState())
+                        {
+                            case FsmStateEnum.Auth:
+                                if (packet.GetMsgType() == MessageTypeEnum.Err)
+                                {
+                                    ClientFsm.SetState(FsmStateEnum.End);
+                                    UdpPacket bye = UdpPacketBuilder.build_bye();
+                                    await SendDataToServer(bye);
+                                }
+                                else if (packet.GetMsgType() == MessageTypeEnum.Reply && packet.ToBytes()[3] == 1)
+                                {
+                                    ClientFsm.SetState(FsmStateEnum.Open);
+                                }
+                                else if (packet.GetMsgType() == MessageTypeEnum.Reply && packet.ToBytes()[3] == 0)
+                                {
+                                    ClientFsm.SetState(FsmStateEnum.Auth);
+                                }
+                                break;
+                            case FsmStateEnum.Open:
+                                if (p.GetMsgType() == MessageTypeEnum.Err)
+                                {
+                                    ClientFsm.SetState(FsmStateEnum.End);
+                                }
+                                else if (p.GetMsgType() == MessageTypeEnum.Bye)
+                                {
+                                    ClientFsm.SetState(FsmStateEnum.End);
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                break;
+            }
+        }
+        Console.Error.WriteLine("Reader terminated...");
     }
-    
+
     public async Task Printer()
     {
-        throw new NotImplementedException();
+        return;
     }
     
-    public async Task SendDataToServer(IPacket packet)
-    {
-        throw new NotImplementedException();
+    // todo add sending func, send from queue, add add to queue func
+    public async Task SendDataToServer(IPacket packet) {
+        byte[] data = packet.ToBytes();
+        switch (ClientFsm.GetState())
+        {
+            case FsmStateEnum.Start:
+                if (packet.GetMsgType() == MessageTypeEnum.Auth)
+                    ClientFsm.SetState(FsmStateEnum.Auth);
+                break;
+        }
+        int id = ((UdpPacket)packet).GetMsgId();
+        for (int i = 0; i < 3; i++)
+        {
+            await _client.Client.SendToAsync(data, SocketFlags.None, endPoint);
+            // wait for confirm message with timeout 250ms
+            int controlId = -1;
+            Task checkTask = Task.Run(() => { controlId = confirmedMessages.Take(); });
+            Task delayTask = Task.Delay(250);
+            Task firstCompleted = await Task.WhenAny(checkTask, delayTask);
+            if (firstCompleted == checkTask && checkTask.IsCompletedSuccessfully && controlId == id)
+            {
+                Console.WriteLine("Message with id " + id + " confirmed");
+                break;
+            }
+        }
     }
 }
--- IpkProject1/udp/UdpPacket.cs
+++ IpkProject1/udp/UdpPacket.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using IpkProject1.enums;
 using IpkProject1.interfaces;
 
@@ -5,18 +6,130 @@ namespace IpkProject1.udp;
 
 public class UdpPacket : IPacket
 {
+    private MessageTypeEnum _msgType;
+    private byte[] _data;
+    private UInt16 _msgId;
+    
+    public UdpPacket(MessageTypeEnum msgType, byte[] data)
+    {
+        _msgType = msgType;
+        _data = data;
+        _msgId = BitConverter.ToUInt16(data[1..3]);
+    }
     public byte[] ToBytes()
     {
-        throw new NotImplementedException();
+        return _data;
+    }
+    
+    public int GetMsgId()
+    {
+        return _msgId;
     }
     
     public void Print()
     {
-        throw new NotImplementedException();
+        if (_msgType == MessageTypeEnum.Reply)
+        {
+            bool result = _data[3] != 0;
+            string message = Encoding.ASCII.GetString(_data[6..^1]);
+            if (result)
+            {
+                Console.Error.WriteLine("Success: " + message);
+            }
+            else
+            {
+                Console.Error.WriteLine("Failure: " + message);
+            }
+        }
+        else if (_msgType == MessageTypeEnum.Msg)
+        {
+            string data = Encoding.ASCII.GetString(_data[3..^1]);
+            string dname = data.Split("\0")[0];
+            string message = data.Split("\0")[1];
+            Console.Error.WriteLine(dname + ": " + message);;
+        }
+        else if (_msgType == MessageTypeEnum.Err)
+        {
+            string data = Encoding.ASCII.GetString(_data[3..^1]);
+            string dname = data.Split("\0")[0];
+            string message = data.Split("\0")[1];
+            Console.Error.WriteLine(dname + ": " + message);
+        }
     }
     
     public MessageTypeEnum GetMsgType()
     {
+        return _msgType;
+    }
+}
+
+public class UdpPacketBuilder : IPacketBuilder
+{
+    public static UdpPacket build_confirm(UInt16 msg_id)
+    {
+        MessageTypeEnum type = MessageTypeEnum.Confirm;
+        byte msg_type_byte = Convert.ToByte((int)type);
+        byte[] msg_id_bytes = BitConverter.GetBytes(msg_id);
+        byte[] data = new byte[1 + 2];
+        data[0] = msg_type_byte;
+        Array.Copy(msg_id_bytes, 0, data, 1, 2);
+        return new UdpPacket(type, data);
+    }
+    public static UdpPacket build_auth(string login, string dname, string secret)
+    {
+        MessageTypeEnum type = MessageTypeEnum.Auth;
+        byte msg_type_byte = Convert.ToByte((int)type);
+        byte[] msg_id_bytes = BitConverter.GetBytes((UInt16)((UdpChatClient)IpkProject1.GetClient()).GetId());
+        byte[] username_bytes = Encoding.ASCII.GetBytes(login);
+        byte[] dname_bytes = Encoding.ASCII.GetBytes(dname);
+        byte[] secret_bytes = Encoding.ASCII.GetBytes(secret);
+        byte[] data = new byte[1 + 2 + username_bytes.Length + 1 + dname_bytes.Length + 1 + secret_bytes.Length + 1];
+        data[0] = msg_type_byte;
+        Array.Copy(msg_id_bytes, 0, data, 1, 2);
+        Array.Copy(username_bytes, 0, data, 3, username_bytes.Length);
+        data[3 + username_bytes.Length] = 0;
+        Array.Copy(dname_bytes, 0, data, 3 + username_bytes.Length + 1, dname_bytes.Length);
+        data[3 + username_bytes.Length + 1 + dname_bytes.Length] = 0;
+        Array.Copy(secret_bytes, 0, data, 3 + username_bytes.Length + 1 + dname_bytes.Length + 1, secret_bytes.Length);
+        data[3 + username_bytes.Length + 1 + dname_bytes.Length + 1 + secret_bytes.Length] = 0;
+        return new UdpPacket(type, data);
+}
+    
+    public static UdpPacket build_msg(string dname, string msg)
+    {
+        MessageTypeEnum type = MessageTypeEnum.Msg;
+        byte msg_type_byte = Convert.ToByte((int)type);
+        byte[] msg_id_bytes = BitConverter.GetBytes((UInt16)((UdpChatClient)IpkProject1.GetClient()).GetId());
+        byte[] dname_bytes = Encoding.ASCII.GetBytes(dname);
+        byte[] msg_bytes = Encoding.ASCII.GetBytes(msg);
+        byte[] data = new byte[1 + 2 + dname_bytes.Length + 1 + msg_bytes.Length + 1];
+        data[0] = msg_type_byte;
+        Array.Copy(msg_id_bytes, 0, data, 1, 2);
+        Array.Copy(dname_bytes, 0, data, 3, dname_bytes.Length);
+        data[3 + dname_bytes.Length] = 0;
+        Array.Copy(msg_bytes, 0, data, 3 + dname_bytes.Length + 1, msg_bytes.Length);
+        data[3 + dname_bytes.Length + 1 + msg_bytes.Length] = 0;
+        return new UdpPacket(type, data);
+    }
+    
+    public static UdpPacket build_error(string dname, string msg)
+    {
         throw new NotImplementedException();
+    }
+    
+    public static UdpPacket build_join(string channel, string dname)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static UdpPacket build_bye()
+    {
+        MessageTypeEnum type = MessageTypeEnum.Bye;
+        byte msg_type_byte = Convert.ToByte((int)type);
+        byte[] msg_id_bytes = BitConverter.GetBytes((UInt16)((UdpChatClient)IpkProject1.GetClient()).GetId());
+        byte[] data = new byte[1 + 2];
+        data[0] = msg_type_byte;
+        Array.Copy(msg_id_bytes, 0, data, 1, 2);
+        return new UdpPacket(type, data);
     }
 }
