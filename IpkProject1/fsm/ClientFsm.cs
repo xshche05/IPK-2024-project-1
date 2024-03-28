e6da1c04b30fa16ca54b1018764b84278b8f95b2
xshche05 <xshche05@stud.fit.vutbr.cz>
2024-03-28 01:13:22 +0100

    Fsm side controls

--- IpkProject1/fsm/ClientFsm.cs
+++ IpkProject1/fsm/ClientFsm.cs
@@ -12,13 +12,19 @@ namespace IpkProject1.fsm;
 public static class ClientFsm
 {
     private static FsmStateEnum _state = FsmStateEnum.Start;
+    public static FsmStateEnum State => _state;
+    
+    private static bool _byeSent = false;
     
     public static void SetState(FsmStateEnum stateEnum)
     {
         var prevState = _state;
         _state = stateEnum;
+        
         Debug.WriteLine($"State changed to: {_state}");
-        if (_state == FsmStateEnum.End && !InputProcessor.CancellationToken.IsCancellationRequested)
+        
+        // Send bye packet if the state is changed to End and bye packet was not sent yet
+        if (_state == FsmStateEnum.End && !InputProcessor.CancellationToken.IsCancellationRequested && !_byeSent)
         {
             InputProcessor.CancellationTokenSource?.Cancel();
             if (!IpkProject1.TimeoutCancellationTokenSource.IsCancellationRequested)
@@ -33,22 +39,59 @@ public static class ClientFsm
                 {
                     Debug.WriteLine("Bye packet sent...");
                     IpkProject1.GetClient().AddPacketToSendQueue(byePacket);
+                    _byeSent = true;
                 }
             }
         }
-
-        if (_state == FsmStateEnum.Open && prevState == FsmStateEnum.Auth)
-        {
-            IpkProject1.AuthSem.Release();
-        }
-        if (_state == FsmStateEnum.Auth && prevState == FsmStateEnum.Auth)
+        
+        // Release semaphore if the state is changed from Auth to Open (REPLY OK)
+        // Release semaphore if the state is changed from Auth to Auth (REPLY NOK)
+        if (_state == FsmStateEnum.Open && prevState == FsmStateEnum.Auth
+            || _state == FsmStateEnum.Auth && prevState == FsmStateEnum.Auth)
         {
             IpkProject1.AuthSem.Release();
         }
     }
-    
-    public static FsmStateEnum GetState()
+
+    public static bool IsCommandAllowed(string command)
     {
-        return _state;
+        if (command == "auth")
+        {
+            if (State != FsmStateEnum.Auth && State != FsmStateEnum.Start)
+            {
+                Io.PrintLine("WARNING: You are already authenticated, cannot authenticate again!", ConsoleColor.Yellow);
+                return false;
+            }
+
+            return true;
+        }
+        if (command == "join")
+        {
+            if (State != FsmStateEnum.Open)
+            {
+                Io.PrintLine("WARNING: You are not authenticated, cannot join a channel!", ConsoleColor.Yellow);
+                return false;
+            }
+            return true;
+        }
+        if (command == "rename")
+        {
+            if (State != FsmStateEnum.Open)
+            {
+                Io.PrintLine("WARNING: You are not authenticated, cannot rename!", ConsoleColor.Yellow);
+                return false;
+            }
+            return true;
+        }
+        if (command == "msg")
+        {
+            if (State != FsmStateEnum.Open)
+            {
+                Io.PrintLine("WARNING: You are not authenticated, cannot send a message!", ConsoleColor.Yellow);
+                return false;
+            }
+            return true;
+        }
+        return false;
     }
 }   
