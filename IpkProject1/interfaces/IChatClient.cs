253b326faff6ba8cc1837a45c38884df5de8df89
xshche05 <xshche05@stud.fit.vutbr.cz>
2024-03-30 15:58:48 +0100

    removed unnecessary task

--- IpkProject1/interfaces/IChatClient.cs
+++ IpkProject1/interfaces/IChatClient.cs
@@ -5,7 +5,7 @@ public interface IChatClient
     public bool _isSenderTerminated { get; set; }
     public void Connect(string host, int port);
     public void Disconnect();
-    public Task AddPacketToSendQueue(IPacket packet);
+    public void AddPacketToSendQueue(IPacket packet);
     public Task Reader();
     public Task Printer();
     public Task Sender();
